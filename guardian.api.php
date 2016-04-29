97bdcf81e6f198903a52912162b639c99424e171
Tessa Bakker <tessa@tessabakker.com>
2016-04-29 13:33:40 +0200

    Guardian API file.

---
+++ guardian.api.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @file
+ * API documentation for the Guardian module.
+ */
+
+/**
+ * Alter the Guardian mail metadata, that will be append to the body text.
+ *
+ * @param array $body
+ *  Content of mail body.
+ */
+function hook_guardian_add_metadata_to_body_alter(&$body) {
+  if (!empty($_SERVER['HTTP_USER_AGENT'])) {
+    $body[] = 'HTTP_USER_AGENT: ' . check_plain($_SERVER['HTTP_USER_AGENT']);
+  }
+}
