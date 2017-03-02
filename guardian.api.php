f40f269857a7be74b2c08542321df1674b6c2db0
Tessa Bakker <tessa@tessabakker.com>
2017-03-02 11:22:29 +0100

    Revert "Issue #2849431 by gmaltoni: Coding Standards"
    This reverts commit 7028dc017d5aaa344ed271685a6e4419a9dcd55b.

--- guardian.api.php
+++ guardian.api.php
@@ -9,9 +9,9 @@
  * Alter the Guardian mail metadata, that will be append to the body text.
  *
  * @param array $body
- *   Content of mail body.
+ *  Content of mail body.
  */
-function hook_guardian_add_metadata_to_body_alter(array &$body) {
+function hook_guardian_add_metadata_to_body_alter(&$body) {
   if (!empty($_SERVER['HTTP_USER_AGENT'])) {
     $body[] = 'HTTP_USER_AGENT: ' . check_plain($_SERVER['HTTP_USER_AGENT']);
   }
