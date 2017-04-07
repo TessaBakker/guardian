ca55ca78a1c1cd41041a7a45c13a86d1b29df22b
Tessa Bakker <tessa@tessabakker.com>
2017-04-07 16:52:42 +0200

    Issue #2867375: Use Guardian with other user profiles

--- guardian.api.php
+++ guardian.api.php
@@ -16,3 +16,26 @@ function hook_guardian_add_metadata_to_body_alter(array &$body) {
     $body[] = 'HTTP_USER_AGENT: ' . check_plain($_SERVER['HTTP_USER_AGENT']);
   }
 }
+
+/**
+ * Set guarded users by Guardian.
+ *
+ * Example format to return:
+ *   array(3 => 'user@example.com');
+ *
+ * Implement _guardian_account_defaults() if some logic is needed for adding
+ * newly guarded users after enabling Guardian.
+ * Set a new password if you need some logic to disable guarded users without
+ * disabling Guardian.
+ *
+ * @return array
+ *  An array of uid's with corresponding e-mail address
+ *  - key must be an integer
+ *  - value must be a valid e-mail address
+ */
+function hook_guardian_guarded_users() {
+  return array(
+    2 => 'user2@example.com',
+    5 => 'user5@example.com',
+  );
+}
